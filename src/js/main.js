96b00936c4b83bb7bc20e9987919ded31a6fac6d
Viktor <viktor.kochegarov@gmail.com>
2019-06-21 20:10:10 +0300

    правки

--- src/js/main.js
+++ src/js/main.js
@@ -1,3 +1,58 @@
+var button = document.querySelector('#button');
+var modal = document.querySelector('#modal');
+var close = document.querySelector('#close');
+
+
+
+button.addEventListener('click', function(){
+  modal.classList.add('modal_active');
+});
+
+close.addEventListener('click', function(){
+  modal.classList.remove('modal_active');
+});
+
+function autoClose(){
+  modal.classList.remove('modal_active');
+}
+
+// setInterval(autoClose, 5000);
+
+$(document).ready(function(){
+  /**
+   * При прокрутке страницы, показываем или срываем кнопку
+   */
+  $(window).scroll(function () {
+      // Если отступ сверху больше 50px то показываем кнопку "Наверх"
+      if ($(this).scrollTop() > 50) {
+          $('#button-up').fadeIn();
+      } else {
+          $('#button-up').fadeOut();
+      }
+  });
+  /** При нажатии на кнопку мы перемещаемся к началу страницы */
+  $('#button-up').click(function () {
+      $('body,html').animate({
+          scrollTop: 0
+      }, 500);
+      return false;
+  });
+  
+});
+
+$(document).ready(function(){
+	$("#menu").on("click","a", function (event) {
+		//отменяем стандартную обработку нажатия по ссылке
+		event.preventDefault();
+		//забираем идентификатор бока с атрибута href
+		var id  = $(this).attr('href'),
+		//узнаем высоту от начала страницы до блока на который ссылается якорь
+			top = $(id).offset().top;
+		//анимируем переход на расстояние - top за 1500 мс
+		$('body,html').animate({scrollTop: top}, 1500);
+	});
+});
+
 $(document).ready(function(){
   var brif = $('.reviews');
   var brifTop = brif.offset().top;
